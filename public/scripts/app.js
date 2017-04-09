9f41ec765cc268d009701d05c39c679b6ca3ea87
Javier J. Alvarez <javierjesusalvarezmontenegro@gmail.com>
2017-04-09 18:27:24 -0500

    Implementacion de Awesomplete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,5 +3,19 @@
 */
 window.onload = function() {
     SearchForm(false);
+    AwesompleteCreate();
 };
+
+function AwesompleteCreate(){
+    var inputSearch = document.getElementById("inputSearch");
+    new Awesomplete(inputSearch, {
+        list: dataRaw.data.map(function (book){
+            return book.title;
+        })
+    });
+    //console.log(_.map(dataRaw.data, function(data){
+    //  return data.title;
+    //}));
+    //console.log(dataRaw.data);
+}
 
